71aafba502d38b6174ce58945a693ca6905ccd35
Michael <comesm@gmail.com>
2017-03-31 15:30:43 -0700

    learning java

---
+++ learningJava/Helloworld.java
@@ -0,0 +1,7 @@
+public class Helloworld {
+  public static void main(String[] args) {
+    String[] names = {"Michael", "John", "Murphy"};
+
+    System.out.println("Hello world, " + names[2]);
+  }
+}
