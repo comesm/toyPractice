b14ec4e6b0b87f9217c427974f58face5a3fa17a
Michael <comesm@gmail.com>
2017-04-21 13:09:35 -0700

    test closures

--- semicolon.js
+++ semicolon.js
@@ -51,6 +51,62 @@ var elementTwo = {
   console.log(typeof func);
   console.log(typeof func());
   console.log(typeof namedFuncExp);
-  console.log(typeof namedFuncExp());
+  //console.log(typeof namedFuncExp());
 
 })();
+
+(function() {
+  var func = function namedFuncExp() {
+    console.log('60', typeof namedFuncExp);
+    return 23;
+  };
+  func();
+})();
+
+// function foo(){} // declaration, since it's part of a <em>Program</em>
+// var bar = function foo(){}; // expression, since it's part of an <em>AssignmentExpression</em>
+
+// new function bar(){}; // expression, since it's part of a <em>NewExpression</em>
+
+// (function(){
+//   function bar(){} // declaration, since it's part of a <em>FunctionBody</em>
+// })();
+
+
+function testing() {
+  console.log('68', typeof testing)
+}
+
+console.log('71',typeof testing);
+
+(function() {
+  var fun = function() {
+    console.log('75', typeof fun);
+  }
+
+  function newFunc() {
+
+  fun();
+  }
+
+newFunc()
+
+})();
+
+//(function(){console.log(6)})()
+
+(function(){
+  var newScope = function scopeExp() {
+      console.log('98', typeof scopeExp);
+      console.log('99', typeof newScope);
+
+      return 23;
+
+  }
+
+  console.log(typeof scopeExp)
+  console.log(typeof newScope)
+})()
+
+
+
