b666c01f1c527134c9041ecd61697e179125d4da
Michael <comesm@gmail.com>
2017-04-24 21:59:15 -0700

    inheriteance and strict

--- prototypalInher.js
+++ prototypalInher.js
@@ -1,3 +1,17 @@
+'use strict';
+
+var michael;
+
+//throws ReferenceError
+  //can't accidentally create global variables
+michael = 5;
+
+//NaN --> non-writeable global variable
+var sum = undefined + 5
+console.log(sum);
+
+var o = {p: 1, p: 2};
+
 function Shape() {
   this.x = 0;
   this.y = 0;
@@ -10,9 +24,10 @@ Shape.prototype.move = function(x, y) {
 }
 
 function Rectangle() {
+  console.log(13, this);
   Shape.call(this);
 }
 
-var s = new Shape();
+var s = new Rectangle();
 
-console.log(s.prototype)
+console.log(s);
